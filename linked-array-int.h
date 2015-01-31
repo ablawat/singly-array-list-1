88e18ccd0504a080a18027e09abc0837eb3d8f38
Adam Bławat <blawat.adam@gmail.com>
2015-01-31 16:05:27 +0100

    Zmieniono nazwy pól struktur, dodano nagłówki funkcji działających na liście.

--- linked-array-int.h
+++ linked-array-int.h
@@ -1,12 +1,15 @@
 #ifndef LINKED_ARRAY_INT_H
 #define LINKED_ARRAY_INT_H
 
-#define VALUES_NUM 8
+#include <stdint.h>
+
+#define NODE_VALUES_NUM 8
+
 
 typedef struct linked_array_int_node
 {
-    int64_t                       values[VALUES_NUM];
-    int64_t                       values_count;
+    int64_t                       values[NODE_VALUES_NUM];
+    int64_t                       values_num;
     
     struct linked_array_int_node  *next;
 }
@@ -14,11 +17,21 @@ linked_array_int_node_t;
 
 typedef struct linked_array_int_head
 {
-    int64_t                       nodes_count;
+    int64_t                       values_num;
     
     struct linked_array_int_node  *node_head;
     struct linked_array_int_node  *node_tail;
 }
 linked_array_int_t;
+
+
+linked_array_int_t *  linked_array_int_create();
+
+int32_t  linked_array_int_add      (linked_array_int_t *list, int64_t new_value);
+
+void     linked_array_int_clear    (linked_array_int_t *list);
+void     linked_array_int_print    (linked_array_int_t *list);
+
+void     linked_array_int_destroy  (linked_array_int_t *list);
 
 #endif
