7d7c0e66633014b190b1b501058f13566e5c6bc9
Adam Bławat <blawat.adam@gmail.com>
2015-02-07 12:08:35 +0100

    Dodano implementacje funkcji linked_array_int_create(), linked_array_int_add() i linked_array_int_print().

---
+++ linked-array-int.c
@@ -0,0 +1,111 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "linked-array-int.h"
+
+linked_array_int_t * linked_array_int_create()
+{
+    linked_array_int_t *new_list = malloc(sizeof(linked_array_int_t));
+    
+    if (new_list != NULL)
+    {
+        new_list -> values_num = 0;
+        
+        new_list -> node_head = NULL;
+        new_list -> node_tail = NULL;
+    }
+    
+    return new_list;
+}
+
+int32_t linked_array_int_add(linked_array_int_t *list, int64_t new_value)
+{
+    int32_t result;
+    
+    if (list -> values_num == 0)
+    {
+        linked_array_int_node_t *new_node = malloc(sizeof(linked_array_int_node_t));
+        
+        if (new_node != NULL)
+        {
+            new_node -> values[0]  = new_value;
+            new_node -> values_num = 1;
+            
+            new_node -> next = NULL;
+            
+            list -> values_num = 1;
+            
+            list -> node_head = new_node;
+            list -> node_tail = new_node;
+            
+            result = 0;
+        }
+        else
+        {
+            result = -1;
+        }
+    }
+    else
+    {
+        if (list -> node_tail -> values_num < NODE_VALUES_NUM)
+        {
+            linked_array_int_node_t *list_node = list -> node_tail;
+            
+            list_node -> values[list_node -> values_num] = new_value;
+            list_node -> values_num += 1;
+            
+            result = 0;
+        }
+        else
+        {
+            linked_array_int_node_t *new_node = malloc(sizeof(linked_array_int_node_t));
+            
+            if (new_node != NULL)
+            {
+                new_node -> values[0]  = new_value;
+                new_node -> values_num = 1;
+                
+                new_node -> next = NULL;
+                
+                list -> node_tail -> next = new_node;
+                list -> node_tail = new_node;
+                
+                result = 0;
+            }
+            else
+            {
+                result = -1;
+            }
+        }
+    }
+    
+    return result;
+}
+
+void linked_array_int_print(linked_array_int_t *list)
+{
+    linked_array_int_node_t *list_node = list -> node_head;
+    int32_t i;
+    
+    printf("L");
+    
+    while (list_node != NULL)
+    {
+        printf("-|");
+        
+        for (i = 0; i < list_node -> values_num; i++)
+        {
+            if (i > 0)
+            {
+                putchar(',');
+            }
+            
+            printf("%ld", list_node -> values[i]);
+        }
+        
+        printf("|");
+        
+        list_node = list_node -> next;
+    }
+    
+    printf("\n");
+}
